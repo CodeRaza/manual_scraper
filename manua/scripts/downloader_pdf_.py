c08904ac5826786ff867d23d48effe842908d9e6
Ali Raza <coder.raza@gmail.com>
2024-03-21 00:04:33 +0500

    changes

--- manua/scripts/downloader_pdf_.py
+++ manua/scripts/downloader_pdf_.py
@@ -44,9 +44,14 @@ def scrape_pdf(base_url, product, manual_name):
             
             # remove_element_by_class('cookie-consent-popup')
             # WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,"//button[contains(@title,'AGREE')]"))).click()
-
+            cookie_close_button = driver.find_element(By.XPATH, '//button[text()="AGREE"]')
+            cookie_close_button.click()
+            
             pdf_div = driver.find_element(By.CSS_SELECTOR, '.viewer-page.viewer-container.active')
+            
 
+            print("Clicked on the Agree button")
+            
             with open('log.txt', 'w') as f:
                 f.write(str(driver.page_source))
 
